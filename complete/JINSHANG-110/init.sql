42ff4014f23a2879890b0e9efc5dfb40a0018bd7
Jokershun <444142748@qq.com>
2015-12-10 17:32:46 +0800

    jinshang-110 添加了条件，删除了转义字符

--- complete/JINSHANG-110/init.sql
+++ complete/JINSHANG-110/init.sql
@@ -41,51 +41,64 @@ dataproc select 1_select
 	 fields: 
 	 (
        (fname:"netweight",alias:"store_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_in_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_out_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_process_amount",type:"double")
+       (fname:"0",alias:"store_in_amount",type:"double"),
+       (fname:"0",alias:"store_out_amount",type:"double"),
+       (fname:"0",alias:"store_process_amount",type:"double")
 	 ),
 	 inputs: "T_GG_SJ_GOODS_dataset",
-	 order_by:("store_process_amount")
+	 order_by:("store_process_amount"),
+	 conditions:" goodsstateno = '110'
+		AND deptid NOT LIKE '03%'
+		AND deptid NOT LIKE 'ZZ%'
+		AND deptid NOT LIKE 'MG%' "
 );
 
 dataproc select 2_select
 (
 	 fields: 
 	 (
-       (fname:"\'0\'",alias:"store_amount",type:"double"),
+       (fname:"0",alias:"store_amount",type:"double"),
        (fname:"netweightsum",alias:"store_in_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_out_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_process_amount",type:"double")
+       (fname:"0",alias:"store_out_amount",type:"double"),
+       (fname:"0",alias:"store_process_amount",type:"double")
 	 ),
 	 inputs: "T_ZY_RK_INBILL_dataset",
-	 order_by:("store_process_amount")
+	 order_by:("store_process_amount"),
+	 conditions:" deptid NOT LIKE '03%'
+		AND deptid NOT LIKE 'ZZ%'
+		AND deptid NOT LIKE 'MG%' "
 );
  
  dataproc select 3_select
 (
 	 fields: 
 	 (
-       (fname:"\'0\'",alias:"store_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_in_amount",type:"double"),
+       (fname:"0",alias:"store_amount",type:"double"),
+       (fname:"0",alias:"store_in_amount",type:"double"),
        (fname:"netweightsum",alias:"store_out_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_process_amount",type:"double")
+       (fname:"0",alias:"store_process_amount",type:"double")
 	 ),
 	 inputs: "T_ZY_CK_OUTBILL_dataset",
-	 order_by:("store_process_amount")
+	 order_by:("store_process_amount"),
+	 conditions:" deptid NOT LIKE '03%'
+		AND deptid NOT LIKE 'ZZ%'
+		AND deptid NOT LIKE 'MG%' "
 );
 
 dataproc select 4_select
 (
 	 fields: 
 	 (
-       (fname:"\'0\'",alias:"store_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_in_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_out_amount",type:"double"),
+       (fname:"0",alias:"store_amount",type:"double"),
+       (fname:"0",alias:"store_in_amount",type:"double"),
+       (fname:"0",alias:"store_out_amount",type:"double"),
        (fname:"preweightsum",alias:"store_process_amount",type:"double")
 	 ),
 	 inputs: "T_ZY_JG_PROCESS_dataset",
-	 order_by:("store_process_amount")
+	 order_by:("store_process_amount"),
+	 conditions:" deptid NOT LIKE '03%'
+		AND deptid NOT LIKE 'ZZ%'
+		AND deptid NOT LIKE 'MG%' "
 );
  
 dataproc union 1_union
