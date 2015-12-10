7f5e9f9004b3672f47eb37042070bfeb21396184
Jokershun <444142748@qq.com>
2015-12-10 17:44:24 +0800

    jinshang-110 del更改路径名，删除转义字符，加上条件

--- complete/JINSHANG-110/del_3.sql
+++ complete/JINSHANG-110/del_3.sql
@@ -4,7 +4,7 @@ begin
 dataset file T_ZY_CK_OUTBILL_dataset_22
 (
 	schema: T_ZY_CK_OUTBILL_schema,
-    filename: "/home/zhaoshun/Data/jingshang_data/T_ZY_CK_OUTBILL_del.txt",
+    filename: "/home/zhaoshun/jinshang110/del/T_ZY_CK_OUTBILL_del.csv",
 	serverid: 0,
 	splitter: (block_size: 10000000)
 );
@@ -14,14 +14,17 @@ dataproc select union_select
 	inputs: "T_ZY_CK_OUTBILL_dataset_22",
 	fields: 
 	 (
-       (fname:"\'0\'",alias:"store_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_in_amount",type:"double"),
+       (fname:"0",alias:"store_amount",type:"double"),
+       (fname:"0",alias:"store_in_amount",type:"double"),
        (fname:"netweightsum",alias:"store_out_amount",type:"double"),
-       (fname:"\'0\'",alias:"store_process_amount",type:"double")
+       (fname:"0",alias:"store_process_amount",type:"double")
 	 ),
 	distinct: true,
 	data_opr:delete,
-    cache:true
+    cache:true,
+	conditions:" deptid NOT LIKE '03%'
+		AND deptid NOT LIKE 'ZZ%'
+		AND deptid NOT LIKE 'MG%' "
 ); 
 
   dataproc statistics T_ZY_CK_OUTBILL_del_statistics
