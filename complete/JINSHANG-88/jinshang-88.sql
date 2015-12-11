e086cc7dc6c2ebe99b393754128371c7bf49f446
Jokershun <444142748@qq.com>
2015-12-11 17:40:27 +0800

    jinshang-88 修改查询

--- complete/JINSHANG-88/jinshang-88.sql
+++ complete/JINSHANG-88/jinshang-88.sql
@@ -1,4 +1,4 @@
-SELECT sum(t.netweightsum),COUNT *
+SELECT sum(t.netweightsum),	COUNT (*)
 FROM t_zy_gh_changeownerlist t
 WHERE R_RECORD_CREATE_DATE>to_date(to_char(sysdate,'yyyy-mm-dd'),'yyyy-mm-dd')
   AND t.jyzxflag=1;
